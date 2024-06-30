bc4a6a0286fe9594a0e641f8b022ce9bd3734906
Kashif56 <8bpcoins4u@gmail.com>
2024-06-30 14:53:17 +0500

    Added Allowed Host

--- GuestHouse/settings.py
+++ GuestHouse/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = False
 SECRET_KEY = 'django-insecure-70sfwr7f4#%)t^z7o(x=2*+r+00zmzck3k*ub(z!o=t3zh$lao'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'https://guesthouse.up.railway.app/', 'guesthouse.up.railway.app']
 
 
 # Application definition
